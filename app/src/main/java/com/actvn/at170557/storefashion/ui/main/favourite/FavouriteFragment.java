127d350a5bc1454f52134c5587372f562493bd2f
NguyenMinhVu <vunm.adr@gmail.com>
2024-09-11 02:00:38 +0700

    Add UI

--- app/src/main/java/com/actvn/at170557/storefashion/ui/main/favourite/FavouriteFragment.java
+++ app/src/main/java/com/actvn/at170557/storefashion/ui/main/favourite/FavouriteFragment.java
@@ -1,66 +1,71 @@
 package com.actvn.at170557.storefashion.ui.main.favourite;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBinding;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.actvn.at170557.storefashion.R;
+import com.actvn.at170557.storefashion.baseapplication.BaseFragment;
+import com.actvn.at170557.storefashion.databinding.FragmentFavouriteBinding;
+import com.actvn.at170557.storefashion.databinding.FragmentSettingsBinding;
+import com.actvn.at170557.storefashion.ui.main.home.adapter.PopularAdapter;
+import com.actvn.at170557.storefashion.ui.main.home.model.ProductItem;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FavouriteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FavouriteFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class FavouriteFragment extends BaseFragment {
+    private FragmentFavouriteBinding binding; // Use the generated binding class
+    private Context context;
 
-    public FavouriteFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavouriteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavouriteFragment newInstance(String param1, String param2) {
-        FavouriteFragment fragment = new FavouriteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public int getLayoutFragment() {
+        return R.layout.fragment_favourite;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    protected ViewBinding inflateBinding(LayoutInflater inflater, ViewGroup container) {
+        binding = FragmentFavouriteBinding.inflate(inflater, container, false);
+        return binding;
+
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+
+        // Initialize RecyclerView using ViewBinding
+        RecyclerView recyclerView = binding.recFavourite;
+        recyclerView.setLayoutManager(new GridLayoutManager(context, 2)); // 2 columns
+
+        // Prepare list items
+        List<ProductItem> itemList = new ArrayList<>();
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+        itemList.add(new ProductItem(R.drawable.sampleimag, "Stussy T-Shirt", "Subtitle 1", "$100"));
+
+        // Set adapter
+        PopularAdapter popularAdapter = new PopularAdapter(context, itemList);
+        recyclerView.setAdapter(popularAdapter);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false);
-    }
 }
